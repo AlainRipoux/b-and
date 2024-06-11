745844f78a0b30e75455ec209dfd9cea1842340c
Ali Ripault <ripault.ali@gmail.com>
2024-06-11 15:39:16 +0200

    show band

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -7,6 +7,9 @@ class BandsController < ApplicationController
     @band = Band.find(params[:id])
     @tasks = @band.tasks
     @band_message = BandMessage.new
+    @second_users = current_user.invites_sent.where(status: "accepted").map { |invite| invite.second_user }
+    @first_users = current_user.invites.where(status: "accepted").map { |invite| invite.first_user }
+    @mates = @second_users + @first_users
     authorize @band
   end
 
@@ -42,6 +45,7 @@ class BandsController < ApplicationController
   def update
     @band = Band.find(params[:id])
     @band.update(band_params)
+    params[:band][:user_id].reject{|u| u == ""}.each {|user_id| UserBand.create(band: @band, user: User.find(user_id.to_i))}
     redirect_to band_path(@band)
     authorize @band
   end
